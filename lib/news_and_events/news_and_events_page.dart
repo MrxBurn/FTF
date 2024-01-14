ee89ec6575bd70b16b6b36a6a08b9bf0d977bb30
MrxBurn <xburngaming@gmail.com>
2024-01-14 18:22:24 +0000

    50% done

--- lib/news_and_events/news_and_events_page.dart
+++ lib/news_and_events/news_and_events_page.dart
@@ -3,6 +3,7 @@ import 'package:ftf/news_and_events/boxing_events.dart';
 import 'package:ftf/news_and_events/fighters_data.dart';
 import 'package:ftf/news_and_events/mma_events.dart';
 import 'package:ftf/reusableWidgets/logo_header.dart';
+import 'package:ftf/styles/styles.dart';
 
 class NewsAndEventsPage extends StatelessWidget {
   const NewsAndEventsPage({super.key});
@@ -15,12 +16,23 @@ class NewsAndEventsPage extends StatelessWidget {
           children: [
             LogoHeader(backRequired: true),
             const FightersData(),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(width: 150, height: 100, child: MMAEvents()),
-                SizedBox(width: 150, height: 100, child: BoxingEvents()),
-              ],
+            Container(
+              decoration:
+                  BoxDecoration(color: const Color(lighterBlack), boxShadow: [
+                BoxShadow(
+                  color: Colors.white.withOpacity(0.3),
+                  spreadRadius: 1,
+                  blurRadius: 7,
+                  offset: const Offset(0, 4),
+                )
+              ]),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(width: 195, child: MMAEvents()),
+                  SizedBox(width: 195, child: BoxingEvents()),
+                ],
+              ),
             )
           ],
         ),
