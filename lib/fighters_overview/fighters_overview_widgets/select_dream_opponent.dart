5996b58d4fcdb2e94d2ba90c5724ce6f731be3d1
MrxBurn <xburngaming@gmail.com>
2024-02-25 16:40:26 +0000

    ChoiceChip done - left thing to do on submit logic

--- lib/fighters_overview/fighters_overview_widgets/select_dream_opponent.dart
+++ lib/fighters_overview/fighters_overview_widgets/select_dream_opponent.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ftf/reusableWidgets/rounded_black_button.dart';
+import 'package:ftf/styles/styles.dart';
 import 'package:ftf/utils/general.dart';
 
 void showDreamOpponent(
@@ -25,6 +27,10 @@ class ChoiceChipList extends StatefulWidget {
 class _ChoiceChipListState extends State<ChoiceChipList> {
   int _selectedIndex = -1;
 
+  Map<String, dynamic> selectedFighter = {};
+
+  Future<void> suggestDreamOpponent() async {}
+
   @override
   void initState() {
     super.initState();
@@ -35,39 +41,89 @@ class _ChoiceChipListState extends State<ChoiceChipList> {
     return Dialog(
       child: SizedBox(
         height: 450,
-        child: ListView.separated(
-          shrinkWrap: true,
-          itemCount: widget.fightersList.length,
-          separatorBuilder: (context, index) => const SizedBox(
-            width: 16,
-          ),
-          itemBuilder: (BuildContext context, idx) {
-            return Padding(
-              padding: const EdgeInsets.only(left: 12.0, right: 12),
-              child: ChoiceChip(
-                avatar: CircleAvatar(
-                  backgroundImage:
-                      widget.fightersList[idx]['profileImageURL'] != ''
-                          ? NetworkImage(
-                              widget.fightersList[idx]['profileImageURL'],
-                            )
-                          : NetworkImage(imgPlaceholder),
-                ),
-                label: SizedBox(
-                  width: double.infinity,
-                  child: Text(
-                    '${widget.fightersList[idx]['firstName']} ${widget.fightersList[idx]['lastName']}',
-                  ),
-                ),
-                selected: _selectedIndex == idx,
-                onSelected: (selectValue) {
-                  setState(() {
-                    _selectedIndex = selectValue ? idx : -1;
-                  });
-                },
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 24,
+            ),
+            ListView.separated(
+              shrinkWrap: true,
+              itemCount: widget.fightersList.length,
+              separatorBuilder: (context, index) => const SizedBox(
+                width: 16,
               ),
-            );
-          },
+              itemBuilder: (BuildContext context, idx) {
+                return Padding(
+                  padding: const EdgeInsets.only(left: 12.0, right: 12),
+                  child: ChoiceChip(
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(
+                          color: _selectedIndex == idx
+                              ? Colors.yellow
+                              : Colors.grey),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    checkmarkColor: Colors.yellow,
+                    color: MaterialStateProperty.all<Color>(Colors.black),
+                    labelStyle: TextStyle(
+                        color: _selectedIndex == idx
+                            ? Colors.yellow
+                            : Colors.white),
+                    avatar: CircleAvatar(
+                      backgroundImage:
+                          widget.fightersList[idx]['profileImageURL'] != ''
+                              ? NetworkImage(
+                                  widget.fightersList[idx]['profileImageURL'],
+                                )
+                              : NetworkImage(imgPlaceholder),
+                    ),
+                    label: SizedBox(
+                      width: double.infinity,
+                      child: Text(
+                        '${widget.fightersList[idx]['firstName']} ${widget.fightersList[idx]['lastName']}',
+                      ),
+                    ),
+                    selected: _selectedIndex == idx,
+                    onSelected: (selectValue) {
+                      setState(() {
+                        _selectedIndex = selectValue ? idx : -1;
+                        if (_selectedIndex == -1) {
+                          selectedFighter = {};
+                        } else {
+                          selectedFighter = widget.fightersList[idx];
+                        }
+                      });
+                    },
+                  ),
+                );
+              },
+            ),
+            const Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: const Text(
+                      'Cancel',
+                      style: TextStyle(
+                          color: Colors.white,
+                          decoration: TextDecoration.underline),
+                    )),
+                BlackRoundedButton(
+                    width: 100,
+                    height: 35,
+                    fontSize: 14,
+                    isLoading: false,
+                    isDisabled: selectedFighter.isEmpty,
+                    onPressed: () {},
+                    text: 'Suggest'),
+                const SizedBox(
+                  width: 24,
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
--- lib/reusableWidgets/rounded_black_button.dart
+++ lib/reusableWidgets/rounded_black_button.dart
@@ -13,6 +13,14 @@ class BlackRoundedButton extends StatelessWidget {
 
   final Color textColour;
 
+  final double? width;
+
+  final double? height;
+
+  final double fontSize;
+
+  final bool isDisabled;
+
   const BlackRoundedButton(
       {super.key,
       required this.isLoading,
@@ -20,34 +28,42 @@ class BlackRoundedButton extends StatelessWidget {
       required this.text,
       this.shadowColour = Colors.red,
       this.icon,
-      this.textColour = Colors.white});
+      this.textColour = Colors.white,
+      this.width,
+      this.height,
+      this.fontSize = 16,
+      this.isDisabled = false});
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        elevation: 5,
-        shadowColor: shadowColour,
+    return SizedBox(
+      width: width,
+      height: height,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          elevation: 5,
+          shadowColor: shadowColour,
+        ),
+        onPressed: !isDisabled ? () => onPressed() : null,
+        child: isLoading == true
+            ? const CircularProgressIndicator()
+            : icon != null
+                ? Wrap(
+                    spacing: 8,
+                    crossAxisAlignment: WrapCrossAlignment.center,
+                    children: [
+                      icon ?? const SizedBox(),
+                      Text(
+                        text,
+                        style: TextStyle(fontSize: 16, color: textColour),
+                      ),
+                    ],
+                  )
+                : Text(
+                    text,
+                    style: TextStyle(fontSize: fontSize),
+                  ),
       ),
-      onPressed: () => onPressed(),
-      child: isLoading == true
-          ? const CircularProgressIndicator()
-          : icon != null
-              ? Wrap(
-                  spacing: 8,
-                  crossAxisAlignment: WrapCrossAlignment.center,
-                  children: [
-                    icon ?? const SizedBox(),
-                    Text(
-                      text,
-                      style: TextStyle(fontSize: 16, color: textColour),
-                    ),
-                  ],
-                )
-              : Text(
-                  text,
-                  style: const TextStyle(fontSize: 16),
-                ),
     );
   }
 }
