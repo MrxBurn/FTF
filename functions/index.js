09a73afa21811e7874ed9126e237162b39e7e57c
MrxBurn <xburngaming@gmail.com>
2024-03-15 17:05:57 +0000

    Notification on offer created (opponentId) implemented

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,14 @@ const admin = require("firebase-admin");
 
 admin.initializeApp();
 
+
+// *********************
+// *********************
+//  FAN NOTIFICATION
+//  WHEN BIO AND WEIGHTCLASS CHANGES
+// *********************
+// *********************
+
 const sendNotificationOnFighterWeightClassChange =
   async (userId, before, after) => {
     const fighterData = await admin
@@ -97,3 +105,57 @@ exports.sendNotificationOnFighterDataChangeToFans = functions.firestore
         sendNotificationOnFighterBioChange(userId, before);
       }
     });
+
+
+// *********************
+// *********************
+//  FAN NOTIFICATION
+//  WHEN FIGHTER RECEIVES OR CREATES A FIGHT
+// *********************
+// *********************
+
+const sendNotificationOnOfferCreate = async (data) => {
+  const fighterData = await admin
+      .firestore()
+      .collection("users")
+      .doc(data.opponentId)
+      .get().then((res) => res.data());
+
+  const followersId = fighterData.followers;
+
+  followersId
+      .forEach((followerId) => {
+        const snapshot = admin
+            .firestore().collection("users").doc(followerId).get();
+        snapshot.then(async (doc) => {
+          if (doc.exists) {
+            const fanData = doc.data();
+
+            if (fanData.deviceToken) {
+              const notification = {
+                token: fanData.deviceToken,
+                notification:
+              {
+                title: "A followed fighter received an offer",
+                body: `${fighterData.firstName} ${fighterData.lastName} has received an offer`,
+              },
+              };
+              try {
+                await admin.messaging().send(notification);
+              } catch (error) {
+                console.error("Error sending notification:", error);
+              }
+            }
+          }
+        });
+      });
+};
+
+exports.sendNotificationToFanFollowedFighterAsOpponent =
+  functions
+      .firestore
+      .document("fightOffers/{offerId}").onCreate(async (snap) => {
+        const data = snap.data();
+
+        sendNotificationOnOfferCreate(data);
+      });
