ae5dab6fc3323909cd312c74fd9b46297400bc30
MrxBurn <xburngaming@gmail.com>
2024-03-17 19:08:09 +0000

    small change

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,8 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
   runApp(const MyApp());
