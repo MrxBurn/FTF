982f7da23e0742aa2a25f74556b0b8d64ec1f5be
alextodea <alex.todea@mer.eco>
2024-07-29 11:26:29 +0100

    Small fix

--- lib/home_pages/fighter_home_page.dart
+++ lib/home_pages/fighter_home_page.dart
@@ -47,116 +47,118 @@ class _FighterHomePageState extends State<FighterHomePage> {
     _firebaseMessaging.requestPermission();
 
     return Scaffold(
-      body: Column(
-        children: [
-          LogoHeader(backRequired: false),
-          StreamBuilder(
-              stream: getUser(),
-              builder: (BuildContext context,
-                  AsyncSnapshot<DocumentSnapshot> snapshot) {
-                if (snapshot.connectionState == ConnectionState.active) {
-                  Map<String, dynamic>? data =
-                      snapshot.data?.data() as Map<String, dynamic>?;
-
-                  if (data?['eula'] == true) {
-                    return Padding(
-                      padding: EdgeInsets.only(left: 24, right: 24),
-                      child: Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              ButtonCard(
-                                path: imagePaths[0],
-                                name: 'Create offer',
-                                route: 'createOfferFighter',
-                              ),
-                              ButtonCard(
-                                path: imagePaths[7],
-                                name: 'Enter offer code',
-                                route: 'enterOfferCodePage',
-                              ),
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 16,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              ButtonCard(
-                                path: imagePaths[6],
-                                name: 'All fighters',
-                                route: 'fightersOverview',
-                                navigate: () => Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => FightersOverview(
-                                        isFighterRoute: true,
-                                      ),
-                                    )),
-                              ),
-                              ButtonCard(
-                                path: imagePaths[2],
-                                route: 'dashboard',
-                                name: 'Dashboard',
-                              ),
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 16,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              ButtonCard(
-                                path: imagePaths[1],
-                                route: 'myOffers',
-                                name: 'My offers',
-                              ),
-                              ButtonCard(
-                                path: imagePaths[3],
-                                route: 'fighterForum',
-                                name: 'Fighter forum',
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 16,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              ButtonCard(
-                                path: imagePaths[4],
-                                route: 'newsEvents',
-                                name: 'Events',
-                              ),
-                              ButtonCard(
-                                path: imagePaths[5],
-                                route: 'myAccountFighter',
-                                name: 'My account',
-                              ),
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 16,
-                          )
-                        ],
-                      ),
-                    );
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            LogoHeader(backRequired: false),
+            StreamBuilder(
+                stream: getUser(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<DocumentSnapshot> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.active) {
+                    Map<String, dynamic>? data =
+                        snapshot.data?.data() as Map<String, dynamic>?;
+        
+                    if (data?['eula'] == true) {
+                      return Padding(
+                        padding: EdgeInsets.only(left: 24, right: 24),
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                ButtonCard(
+                                  path: imagePaths[0],
+                                  name: 'Create offer',
+                                  route: 'createOfferFighter',
+                                ),
+                                ButtonCard(
+                                  path: imagePaths[7],
+                                  name: 'Enter offer code',
+                                  route: 'enterOfferCodePage',
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 16,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                ButtonCard(
+                                  path: imagePaths[6],
+                                  name: 'All fighters',
+                                  route: 'fightersOverview',
+                                  navigate: () => Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => FightersOverview(
+                                          isFighterRoute: true,
+                                        ),
+                                      )),
+                                ),
+                                ButtonCard(
+                                  path: imagePaths[2],
+                                  route: 'dashboard',
+                                  name: 'Dashboard',
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 16,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                ButtonCard(
+                                  path: imagePaths[1],
+                                  route: 'myOffers',
+                                  name: 'My offers',
+                                ),
+                                ButtonCard(
+                                  path: imagePaths[3],
+                                  route: 'fighterForum',
+                                  name: 'Fighter forum',
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 16,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                ButtonCard(
+                                  path: imagePaths[4],
+                                  route: 'newsEvents',
+                                  name: 'Events',
+                                ),
+                                ButtonCard(
+                                  path: imagePaths[5],
+                                  route: 'myAccountFighter',
+                                  name: 'My account',
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 16,
+                            )
+                          ],
+                        ),
+                      );
+                    } else {
+                      return EULAPage(
+                        user: data,
+                      );
+                    }
                   } else {
-                    return EULAPage(
-                      user: data,
+                    return Center(
+                      child: CircularProgressIndicator(),
                     );
                   }
-                } else {
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-              }),
-        ],
+                }),
+          ],
+        ),
       ),
     );
   }
