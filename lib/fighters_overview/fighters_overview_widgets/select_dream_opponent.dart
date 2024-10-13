b8505bef573746917aa8d1b9affcc8d20ef1f266
MrxBurn <xburngaming@gmail.com>
2024-10-13 10:30:39 +0100

    Fixed suggest dream opponent

--- lib/fighters_overview/fighters_overview_widgets/select_dream_opponent.dart
+++ lib/fighters_overview/fighters_overview_widgets/select_dream_opponent.dart
@@ -78,63 +78,66 @@ class _ChoiceChipListState extends State<ChoiceChipList> {
   Widget build(BuildContext context) {
     return Dialog(
       child: SizedBox(
-        height: 450,
+        height: 500,
         child: Column(
           children: [
             const SizedBox(
               height: 24,
             ),
-            ListView.separated(
-              shrinkWrap: true,
-              itemCount: widget.fightersList.length,
-              separatorBuilder: (context, index) => const SizedBox(
-                width: 16,
-              ),
-              itemBuilder: (BuildContext context, idx) {
-                return Padding(
-                  padding: const EdgeInsets.only(left: 12.0, right: 12),
-                  child: ChoiceChip(
-                    shape: RoundedRectangleBorder(
-                      side: BorderSide(
+            SizedBox(
+              height: 400,
+              child: ListView.separated(
+                shrinkWrap: true,
+                itemCount: widget.fightersList.length,
+                separatorBuilder: (context, index) => const SizedBox(
+                  width: 16,
+                ),
+                itemBuilder: (BuildContext context, idx) {
+                  return Padding(
+                    padding: const EdgeInsets.only(left: 12.0, right: 12),
+                    child: ChoiceChip(
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(
+                            color: _selectedIndex == idx
+                                ? Colors.yellow
+                                : Colors.grey),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      checkmarkColor: Colors.yellow,
+                      color: WidgetStateProperty.all<Color>(Colors.black),
+                      labelStyle: TextStyle(
                           color: _selectedIndex == idx
                               ? Colors.yellow
-                              : Colors.grey),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    checkmarkColor: Colors.yellow,
-                    color: WidgetStateProperty.all<Color>(Colors.black),
-                    labelStyle: TextStyle(
-                        color: _selectedIndex == idx
-                            ? Colors.yellow
-                            : Colors.white),
-                    avatar: CircleAvatar(
-                      backgroundImage:
-                          widget.fightersList[idx]['profileImageURL'] != ''
-                              ? NetworkImage(
-                                  widget.fightersList[idx]['profileImageURL'],
-                                )
-                              : NetworkImage(imgPlaceholder),
-                    ),
-                    label: SizedBox(
-                      width: double.infinity,
-                      child: Text(
-                        '${widget.fightersList[idx]['firstName']} ${widget.fightersList[idx]['lastName']}',
+                              : Colors.white),
+                      avatar: CircleAvatar(
+                        backgroundImage:
+                            widget.fightersList[idx]['profileImageURL'] != ''
+                                ? NetworkImage(
+                                    widget.fightersList[idx]['profileImageURL'],
+                                  )
+                                : NetworkImage(imgPlaceholder),
                       ),
+                      label: SizedBox(
+                        width: double.infinity,
+                        child: Text(
+                          '${widget.fightersList[idx]['firstName']} ${widget.fightersList[idx]['lastName']}',
+                        ),
+                      ),
+                      selected: _selectedIndex == idx,
+                      onSelected: (selectValue) {
+                        setState(() {
+                          _selectedIndex = selectValue ? idx : -1;
+                          if (_selectedIndex == -1) {
+                            selectedFighter = {};
+                          } else {
+                            selectedFighter = widget.fightersList[idx];
+                          }
+                        });
+                      },
                     ),
-                    selected: _selectedIndex == idx,
-                    onSelected: (selectValue) {
-                      setState(() {
-                        _selectedIndex = selectValue ? idx : -1;
-                        if (_selectedIndex == -1) {
-                          selectedFighter = {};
-                        } else {
-                          selectedFighter = widget.fightersList[idx];
-                        }
-                      });
-                    },
-                  ),
-                );
-              },
+                  );
+                },
+              ),
             ),
             const Spacer(),
             Row(
